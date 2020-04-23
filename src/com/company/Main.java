a6b6cb58d53c2bdc37c112a90670bc15249eadd0
rollnick <rollnicker@mail.ru>
2020-04-23 21:05:26 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x;
+        for (x = 1; x <= 50; x++) {
+            System.out.println(x);
+        }
+    }
+}
